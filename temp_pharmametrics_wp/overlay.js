f677abfca7bfcbf1799594b794b01f14842b3c36
laurailona <clarkelaura92@gmail.com>
2019-07-14 14:26:46 +0200

    Toggle overlay

--- temp_pharmametrics_wp/overlay.js
+++ temp_pharmametrics_wp/overlay.js
@@ -2,9 +2,18 @@
 
 var disclaimerOverlay = document.querySelector('#disclaimer');
 var hideOverlayButton = document.querySelector('#close-disclaimer');
+var showDisclaimerButton = document.querySelector('#show-disclaimer');
 
 
 function hideDisclaimer() {
-  console.log("Correct elemets are selected.");
+  disclaimerOverlay.setAttribute("style", "display:none");
+  hideOverlayButton.setAttribute("style", "display:none");
 }
+
+function showDisclaimer() {
+  disclaimerOverlay.setAttribute("style", "display:block");
+  hideOverlayButton.setAttribute("style", "display:block");
+}
+
 hideOverlayButton.addEventListener("click", hideDisclaimer);
+showOverlayButton.addEventListener("click", showDisclaimer);
