6e377e0ed4eb56eafae856d633eec92467fb3723
laurailona <clarkelaura92@gmail.com>
2019-07-14 14:12:46 +0200

    Add overlay, add js file for overlay

---
+++ temp_pharmametrics_wp/overlay.js
@@ -0,0 +1,10 @@
+/* Overlay Button (Disclaimer) */
+
+var disclaimerOverlay = document.querySelector(#disclaimer);
+var hideOverlayButton = document.querySelector(#close-disclaimer);
+
+
+function hideDisclaimer() {
+  console.log("Correct elemets are selected.");
+}
+hideOverlayButton.addEventListener("click", hideDisclaimer);
