3fd7208c0cb53ffa5bea9a79105b8912ff79d407
laurailona <clarkelaura92@gmail.com>
2019-07-14 14:45:23 +0200

    Fullscreen overflow

--- temp_pharmametrics_wp/overlay.js
+++ temp_pharmametrics_wp/overlay.js
@@ -3,16 +3,19 @@
 var disclaimerOverlay = document.querySelector('#disclaimer');
 var hideOverlayButton = document.querySelector('#close-disclaimer');
 var showDisclaimerButton = document.querySelector('#show-disclaimer');
+var body = document.querySelector('body');
 
 
 function hideDisclaimer() {
   disclaimerOverlay.setAttribute("style", "display:none");
   hideOverlayButton.setAttribute("style", "display:none");
+  body.setAttribute("style", "overflow:auto");
 }
 
 function showDisclaimer() {
   disclaimerOverlay.setAttribute("style", "display:block");
   hideOverlayButton.setAttribute("style", "display:block");
+  body.setAttribute("style", "overflow:hidden");
 }
 
 hideOverlayButton.addEventListener("click", hideDisclaimer);
