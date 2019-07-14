fca9d70a55e5cc38e230f9f02d351a34c9ae8e08
laurailona <clarkelaura92@gmail.com>
2019-07-14 19:36:00 +0200

    Remove speech bubble and update skills

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
-   var profilePicture = document.getElementById("profile-picture");
-   var speechBubble = document.getElementById("bubble");
-
-   function moveProfilePicture() {
-     profilePicture.setAttribute("style", "bottom: -2vw");
-   }
-
-   function fadeInBubble() {
-     speechBubble.setAttribute("style", "visibility: visible; opacity: 1")
-   }
-
-   setTimeout(moveProfilePicture, 1000);
-   setTimeout(fadeInBubble, 2000);
-
-   function customTypeWriter() {
-     typeWriterEffect(["Hello there. I'm Laura and I'm a frontend web developer with backend knowledge. I'm passionate about JavaScript, clean code and creative UI."], "twe-typed-text", false);
-   }
-   setTimeout(customTypeWriter, 2500);
-
-//Show-Hide top navigation
+   //Show-Hide top navigation
   var showMenuButton = document.getElementById("show-menu-button");
   var topNav = document.getElementById("top-nav");
   var menuVisible = false;
